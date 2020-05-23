44e2457e37d19a9991f6733807a32ca2db9527c0
utkugokcen13 <utku.gokcen13@gmail.com>
2020-05-23 11:20:57 +0300

    Update createclubpage.java

--- app/src/main/java/com/utkugokcen/biact/createclubpage.java
+++ app/src/main/java/com/utkugokcen/biact/createclubpage.java
@@ -8,13 +8,17 @@ import android.view.View;
 import android.widget.TextView;
 
 public class createclubpage extends AppCompatActivity {
-    String clubname, name, surname, department, email;
-    TextView clubnameText;
+    String clubname, name, surname, department, email, clubdescription, namesurnameText, password;
+    TextView clubnameText, clubdescriptionText, namesurname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.createclubpagelayout);
+
         clubnameText = findViewById(R.id.clubnameText);
+        clubdescriptionText = findViewById(R.id.clubdescriptionText);
+        namesurname = findViewById(R.id.namesurnameT);
+
 
         Intent intent = getIntent();
         name = intent.getStringExtra("name");
@@ -22,17 +26,28 @@ public class createclubpage extends AppCompatActivity {
         department = intent.getStringExtra("dep");
         email = intent.getStringExtra("mail");
         clubname = intent.getStringExtra("clubname");
+        clubdescription = intent.getStringExtra("clubdes");
+        namesurnameText = intent.getStringExtra("namesurname");
+        password = intent.getStringExtra("password");
+
+        namesurname.setText(namesurnameText);
+
+
 
    }
 
     public void createclub1Clicked(View view){
         clubname = clubnameText.getText().toString();
+        clubdescription = clubdescriptionText.getText().toString();
         Intent intent = new Intent(createclubpage.this, clubfeedpage.class);
-        intent.putExtra("clubname", clubname);
         intent.putExtra("name", name);
         intent.putExtra("surname", surname);
         intent.putExtra("dep", department);
         intent.putExtra("mail", email);
+        intent.putExtra("clubname", clubname);
+        intent.putExtra("clubdes", clubdescription);
+        intent.putExtra("namesurname", namesurnameText);
+        intent.putExtra("password", password);
 
         startActivity(intent);
     }
@@ -48,6 +63,9 @@ public class createclubpage extends AppCompatActivity {
         intent.putExtra("dep", department);
         intent.putExtra("mail", email);
         intent.putExtra("clubname", clubname);
+        intent.putExtra("clubdes", clubdescription);
+        intent.putExtra("namesurname", namesurnameText);
+        intent.putExtra("password", password);
         startActivity(intent);
     }
 }
